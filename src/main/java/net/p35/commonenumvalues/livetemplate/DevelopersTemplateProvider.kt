e66bd9e82afcdc2a586fe1ab1cf64f2574b73309
Pavlo Zhukov <pahazhukov@gmail.com>
2019-05-04 13:12:37 +0300

    Add Developers live template group with template examples

---
+++ src/main/java/net/p35/commonenumvalues/livetemplate/DevelopersTemplateProvider.kt
@@ -0,0 +1,13 @@
+package net.p35.commonenumvalues.livetemplate
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
+
+class DevelopersTemplateProvider : DefaultLiveTemplatesProvider {
+    override fun getDefaultLiveTemplateFiles(): Array<String> {
+        return arrayOf("liveTemplates/Markdown")
+    }
+
+    override fun getHiddenLiveTemplateFiles(): Array<String>? {
+        return arrayOf()
+    }
+}
